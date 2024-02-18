a43971bb65715cad7f4e555ba58b9c058885c9da
bdok23 <bdokmeci@gmail.com>
2024-02-17 16:13:18 -0800

    file changes

--- createType.js
+++ createType.js
@@ -1,7 +1,7 @@
 const options = {
     method: "POST",
     headers: {
-      "X-API-KEY": "YOUR_API_KEY",
+      "X-API-KEY": "sk_production_9tW6CCCcaf24zi66M2GcoxfKaVBRWGaZ9KSSGzCUiafxriJVQ6U1DAD2asiyS4gwgojTGdYx2nBfj3vZ3JzE9GQskEb6wd2vmbgCMje3ZcPGieKGogaqtGWaiZ65DjEcpN8s7kjXCpotVTfcrTs9dAjdmxAXJRdELgJqg7QKBaSFQb8NB9Lh5ovGYUoP9oNjBvrXy5F1akskvnoHHS7EpbC9",
       "Content-Type": "application/json",
     },
     body: '{"credentialSubjectSchema":[{"name":"course","type":"string"},{"name":"passed","type":"bool"}]}',
